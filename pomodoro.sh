a98d2336277055c11fb61f4b75ea072651f1797a
Andy Wendt <andy@awendt.com>
2015-09-14 14:24:29 -0600

    initial commit

---
+++ pomodoro.sh
@@ -0,0 +1,12 @@
+echo "Pomodoro Secords Left"
+secs=$((25 * 60))
+while [ $secs -gt 0 ]; do
+
+   echo -ne "\e[32;1m$secs\e[0m\033[0K\r"
+   sleep 1
+   : $((secs--))
+   if [ "$secs" == 0 ]; then
+	notify-send --urgency="critical" "Pomodoro Finished"
+   fi
+done
+echo "Pomodoro Finished"
